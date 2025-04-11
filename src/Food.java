1b0a8435bf67a07eff714d6c6c5e2aee3f0f9ea7
ivasx <ambroziak.v.ivan@gmail.com>
2025-04-11 21:36:27 +0300

    Update Food.java

--- src/Food.java
+++ src/Food.java
@@ -2,4 +2,34 @@ public class Food {
     private String name;
     private kindFood kind;
     private int satiety;
+
+    public Food(String name, kindFood kind, int satiety) {
+        this.name = name;
+        this.kind = kind;
+        this.satiety = satiety;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public kindFood getKind() {
+        return kind;
+    }
+
+    public void setKind(kindFood kind) {
+        this.kind = kind;
+    }
+
+    public int getSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(int satiety) {
+        this.satiety = satiety;
+    }
 }
